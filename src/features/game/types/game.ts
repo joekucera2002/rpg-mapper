d133215a4827db9e1cd5303198f413f070c07499
Joe Kucera <joekucera2002@gmail.com>
2026-06-04 12:23:52 -0700

    Updating ci, removing dead code.

--- src/features/game/types/game.ts
+++ src/features/game/types/game.ts
@@ -1,23 +1,6 @@
-export interface GameRules {
-  effects: string[];
-  walls: string[];
-  markers: string[];
-}
-
 export interface Game {
   id: string;
   name: string;
-  // color: string;
-  // image: string | null;
-  // rules: GameRules;
   lastPlayed: number;
   createdAt: number;
-}
-
-export function defaultRules(): GameRules {
-  return {
-    effects: [],
-    walls: [],
-    markers: [],
-  };
 }
